29978cf3b8c48c21156bec403db82f01fd763e64
Kim Pham <pkduongsu@gmail.com>
2025-06-11 23:32:05 +1000

    10/6/25

---
+++ SQL_50/queries_quality_and_percentage_easy.sql
@@ -0,0 +1,4 @@
+SELECT query_name, ROUND(SUM(rating / position) / COUNT(query_name), 2) as quality, 
+ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)  / COUNT(query_name)) * 100, 2) as poor_query_percentage
+FROM Queries
+GROUP BY query_name
