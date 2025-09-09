b5d0e15cab56b416ceefb69e7af4b2de02fee7d3
Kim Pham <pkduongsu@gmail.com>
2025-09-10 00:33:16 +1000

    9/9/25

--- Python/lowest_common_ancestor_BST_med.py
+++ Python/lowest_common_ancestor_BST_med.py
@@ -5,6 +5,7 @@ def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> Tree
             if p.val > current.val and q.val > current.val:
                 current = current.right #both are on right hand side, go down first node on left
             elif p.val < current.val and q.val < current.val:
-                current = current.left
-            else:
+                current = current.left #both are on left hand side, go down first node on left
+            else: #when one is root, (p.val == current.val or q.val == current.val) we can just return root as it will always be the lowest ancestor possible
+                #or when theres a split, p.val < current.val and q.val > current.val -> lowest ancestor is the split point, we can just return it now.      
                 return current
