d61e7a5488f9e004676cc9374108385097efd1f8
Kim Pham <pkduongsu@gmail.com>
2025-09-10 14:32:18 +1000

    10/9/25

--- Python/3_Sum_med.py
+++ Python/3_Sum_med.py
@@ -16,4 +16,33 @@ class Solution:
 
         return [list(i) for i in res] #return type needs to be nested list, added tuples -> convert each subarray to list
     
-    #Optimal solution
+#Optimal solution
+#Space O (m) - number of triplets
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #better, O(n^2) solution:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]: #as nums is sorted, same values will stay near together
+                continue
+            
+            #for each first value, we find the other 2 values using two pointers
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0: #as now the array is sorted, to reduce the sum value, go to smaller values towards left
+                    r = r - 1
+                elif threeSum < 0:
+                    l = l + 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    #update pointer -> shift the left pointer
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r: #update the left pointer till its not the duplicate value, and maintain l < r ofc
+                        l += 1
+
+        return res
+
