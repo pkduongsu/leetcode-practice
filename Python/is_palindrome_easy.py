0adb475d0be0a1af96046adc958d3fbc12169b57
Kim Pham <pkduongsu@gmail.com>
2025-09-22 23:33:40 +1000

    22/9/25

--- Python/is_palindrome_easy.py
+++ Python/is_palindrome_easy.py
@@ -1,19 +1,38 @@
 #my first solution:
-#Complexity: O(n)
-#can be improved
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        chars = [char for char in str(x)]
+#Complexity: Time: O(n), Space: O(2n) -> O(n)
+#can be improved in terms of space complexity
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         rev = []
-        for i in range(0, len(chars)):
-            rev.append(chars[(len(chars) - 1) - i])
+        s_list = [char.lower() for char in s if char.isalnum()]
+        print(s_list)
+        for i in range(len(s) - 1, -1, -1): #how to loop backwards till the last char in python
+            if s[i].isalnum():
+                rev.append(s[i].lower())
 
-        if chars == rev:
-            return True
 
-        return False
+        print(rev)
+
+#Time: O(n) - looping through every char in the string -> n: maximum length of the string
+#Space: O(1) - only storing 2 variables - l, r
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+
+        while l <= r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
         
+        return True
