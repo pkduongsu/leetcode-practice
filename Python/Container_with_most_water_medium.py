de5e22052a0fe11d48a28c497b8f1d8eba6b79ef
Kim Pham <pkduongsu@gmail.com>
2025-09-24 13:08:40 +1000

    24/09/25

--- Python/Container_with_most_water_medium.py
+++ Python/Container_with_most_water_medium.py
@@ -1,19 +1,25 @@
-#Brute force: go through every possible combinations of columns and find the maximum area possible
-#iterate through array twice -> O(n^2)
+#Brute force:
+#Time complexity: go through array twice in a nested loop -> O(n^2) where n is the maximum length of the heights array
+#Space complexity: sotring only maxArea, current Area -> O(1)
+
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        currentMax = 0
-        
-        for i in range(0, len(heights)):
-            for j in range(i+1, len(heights)):
-                #area of rect: width * height
+        #define in the problem: output is the maximum area that we can achieve
+        #Area of a rectangle: height * width
+        #height: constrained by the lower bar -> get the lower bar using min func
+        #width: distance between 2 bars -> index of the further, rightmost bar - index of leftmost one (or just use abs)
+        maxArea = 0
+        for i in range(len(heights)):
+            for j in range(i + 1, len(heights)):
                 currentArea = min(heights[i], heights[j]) * (j - i)
-                currentMax = max(currentMax, currentArea)
+                maxArea = max(maxArea, currentArea)
 
-        return currentMax
+        return maxArea
+
     
 #optimize: use two pointers, with the idea of maximizing the possible area by finding a higher height if one col is being the constraint (lower height -> lower min(r, l) -> smaller area)
 #iterate through array once -> O(n)
+#Space: O(1)
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
         l, r = 0, len(heights) - 1
