02d3efe077f413294cea1f4864f95e51631706cf
Kim Pham <pkduongsu@gmail.com>
2025-10-15 11:05:21 +1100

    15/10/25

--- Python/Longest_substr_without_repeating_characters_med.py
+++ Python/Longest_substr_without_repeating_characters_med.py
@@ -1,35 +1,47 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #input: string s
-        #output: length of the longest substring without repeating characters - int
-        #def lenghtofLongestSub(s: str) - > int:
+       #input: string s
+       #output: length of the longest substring without any duplicates -> integer
+       #define function: lengthOfLongestSubstring(s: str) -> int:
 
-        #brute force:
-        #to have a list to track the characters that we went through
-        #if there is a duplicate character, stop
-        #find the length
-        #and keep a variable to track the maximum length we can achieve for non-repeating characters
+       #test 1: input: zxyzxyz -> output: 3
+       #test 2: input: abcdab -> output: 4
+       #test 3: input: xxxx -> output: 1
 
-        # maxLen = 0
+       #brute force approach:
+       #loop through each character
+       #set() 
+       #loop through the remaining characters for the current char we're at
+       #see the longest possible sequence that we can form with that char
+       #variable to store the max length and update that by comparing the current max length and the 
+       #stored max length
+       #finally, return maxLength
 
-        # for i in range(len(s)):
-        #     subStr = []
-        #     for j in range(i, len(s)):
-        #         if s[j] in subStr:
-        #             break
-        #         subStr.append(s[j])
-        #     maxLen = max(maxLen, len(subStr))
-        # return maxLen
+    #    maxLength = 0
+    #    #question: constraint -> 0 <= s.length <= ...
+    #    for i in range(len(s)):
+    #         charSet = set()
+    #         for j in range(i, len(s)):
+    #             if s[j] in charSet:
+    #                 break
+    #             charSet.add(s[j])
+    #         maxLength = max(maxLength, len(charSet))
 
-        #Time complexity O(n * m) : where n is the length of the string, and m is the length of the longest substring without repeating characters
-        #Space: O(n) - worst case have to store the whole string
+    #    return maxLength
 
-        #how can i improve
-        #sliding window?
-        #how is sliding window implemented?
+        #Time complexity: O(n * m) where n is the length of the input string, m is the number of unique contiguous characters in the string
+        #Space complexity: O(m) m is the number of unique contiguous characters in the string
+       
+        #sliding window algorithm, O(n)
+        maxLength = 0
         l = 0
-        charSet  = set()
-        maxLen = 0
+        charSet = set()
+
+        #while executing this sliding window algo, if we actually find a duplicate
+        #we're just gonna update the charSet by removing the old elements
+
+        #input: zxyzxyz -> output: 3
+        #input: abcdab -> output: 4
 
         for r in range(len(s)):
             while s[r] in charSet:
@@ -37,13 +49,9 @@ class Solution:
                 l += 1
 
             charSet.add(s[r])
-            maxLen = max(maxLen, r - l + 1)
+            maxLength = max(maxLength, len(charSet))
 
-        return maxLen
+        return maxLength
 
-        #Time: O(n) where n is the length of the string
-        #Space: O(m) where m is the number of unique characters in string
-
-
-
-        
+        #Time complexity: O(n)
+        #Space: O(m) 
