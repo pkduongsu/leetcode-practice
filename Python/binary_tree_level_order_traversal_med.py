c3b671e125e8f0464d289d58bdeab70c6e45e450
Kim Pham <pkduongsu@gmail.com>
2025-10-27 16:24:32 +1100

    27/10/25

--- Python/binary_tree_level_order_traversal_med.py
+++ Python/binary_tree_level_order_traversal_med.py
@@ -1,5 +1,14 @@
 #BFS: 
+from collections import deque
+from typing import Optional, List
 # time complexity O(n), memory: O(n)
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         queue = deque()
